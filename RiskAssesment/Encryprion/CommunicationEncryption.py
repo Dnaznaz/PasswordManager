67645f3ae5eafe7c287103e9f67a5205eb9abcb7
Daniel Vasershtein <danielvase@gmail.com>
2017-03-06 11:52:22 +0200

    Add encryption test grounds

---
+++ RiskAssesment/Encryprion/CommunicationEncryption.py
@@ -0,0 +1,8 @@
+import unittest
+
+# Tests
+class CommunicationEncyptionTests(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ RiskAssesment/Encryprion/DatabaseEncryption.py
@@ -0,0 +1,13 @@
+import unittest
+
+# Tests
+class DatabaseEncyptionTests(unittest.TestCase):
+
+    def testIsEnctypted(self):
+        pass
+
+    def test(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
